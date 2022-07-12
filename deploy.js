8ebe9361cda1c7af912b226113552f9cc84a0d13
pfed-prog <fedorovn19@gmail.com>
2022-07-12 08:04:07 +0200

    improved formatting

--- deploy.js
+++ deploy.js
@@ -1,36 +1,48 @@
-import { makeApplicationCreateTxnFromObject,OnApplicationComplete, waitForConfirmation } from "algosdk";
+import {
+  makeApplicationCreateTxnFromObject,
+  OnApplicationComplete,
+  waitForConfirmation,
+} from "algosdk";
 import fs from "fs";
 
 import { algodClient } from "./utils.js";
 import { user } from "./config.js";
 
 export const deploy = async () => {
-    const suggestedParams = await algodClient.getTransactionParams().do();
+  const suggestedParams = await algodClient.getTransactionParams().do();
 
-    const app = fs.readFileSync(new URL("../AlgoDepo/contracts/deposit_approval.teal", import.meta.url), "utf8");
-    const compileApp = await algodClient.compile(app).do();
+  const app = fs.readFileSync(
+    new URL("../AlgoDepo/contracts/deposit_approval.teal", import.meta.url),
+    "utf8"
+  );
+  const compileApp = await algodClient.compile(app).do();
 
-    const clearState = fs.readFileSync(new URL("../AlgoDepo/contracts/deposit_clear_state.teal", import.meta.url), "utf8");
-    const compiledClearProg = await algodClient.compile(clearState).do();
-  
-    const tx = makeApplicationCreateTxnFromObject({
-      suggestedParams,
-      from: user.addr,
-      approvalProgram: new Uint8Array(Buffer.from(compileApp.result, "base64")),
-      clearProgram: new Uint8Array(Buffer.from(compiledClearProg.result, "base64")),
-      numGlobalByteSlices: 0,
-      numGlobalInts: 0,
-      numLocalByteSlices: 0,
-      numLocalInts: 0,
-      onComplete: OnApplicationComplete.NoOpOC,
-    });
+  const clearState = fs.readFileSync(
+    new URL("../AlgoDepo/contracts/deposit_clear_state.teal", import.meta.url),
+    "utf8"
+  );
+  const compiledClearProg = await algodClient.compile(clearState).do();
 
-    let txSigned = tx.signTxn(user.sk);
-    const { txId } = await algodClient.sendRawTransaction(txSigned).do();
-    const transactionResponse = await waitForConfirmation(algodClient, txId, 5);
-    const appId = transactionResponse["application-index"];
-    
-    console.log("Created app-id: ", appId);
+  const tx = makeApplicationCreateTxnFromObject({
+    suggestedParams,
+    from: user.addr,
+    approvalProgram: new Uint8Array(Buffer.from(compileApp.result, "base64")),
+    clearProgram: new Uint8Array(
+      Buffer.from(compiledClearProg.result, "base64")
+    ),
+    numGlobalByteSlices: 0,
+    numGlobalInts: 0,
+    numLocalByteSlices: 0,
+    numLocalInts: 0,
+    onComplete: OnApplicationComplete.NoOpOC,
+  });
 
-    return appId
-}
+  let txSigned = tx.signTxn(user.sk);
+  const { txId } = await algodClient.sendRawTransaction(txSigned).do();
+  const transactionResponse = await waitForConfirmation(algodClient, txId, 5);
+  const appId = transactionResponse["application-index"];
+
+  console.log("Created app-id: ", appId);
+
+  return appId;
+};
